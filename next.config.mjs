de37c17405891e167db6effc19eb03431333076f
ReelForge Developer <developer@reelforge.ai>
2026-06-15 14:55:54 -0400

    chore: add default next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If you are deploying on a static-only web host, uncomment the line below:
+  // output: 'export',
+  // images: { unoptimized: true }, // Required if output is 'export'
+};
+
+export default nextConfig;
